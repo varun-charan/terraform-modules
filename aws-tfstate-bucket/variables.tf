611c68f6eb05d5b39f75c3e9cdb9a31c1570adff
Charan, Varun <varun.charan@barco.com>
2022-10-21 06:16:09 +0100

    Ran terraform fmt for aws-tfstate-bucket module

--- aws-tfstate-bucket/variables.tf
+++ aws-tfstate-bucket/variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "The AWS region to deploy to (e.g. eu-west-1)"
-  type = string
+  type        = string
 }
 
 variable "aws_account_id" {
   description = "AWS account to hold the state file buckets."
-  type = string
+  type        = string
 }
 
 variable "bucket_name" {
   description = "Bucket name"
-  type = string
+  type        = string
 }
